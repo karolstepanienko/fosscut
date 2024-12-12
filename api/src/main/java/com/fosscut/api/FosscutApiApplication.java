eee07aa321f89d530792669a5b47b758c85989ad
karolstepanienko <karol.stepanienko@gmail.com>
2024-12-13 00:31:43 +0100

    feat: simple spring boot api

---
+++ api/src/main/java/com/fosscut/api/FosscutApiApplication.java
@@ -0,0 +1,13 @@
+package com.fosscut.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FosscutApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FosscutApiApplication.class, args);
+    }
+
+}
