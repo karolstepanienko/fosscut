be719f6e119747525a216cb34a4166ebb2e4666f
karolstepanienko <karol.stepanienko@gmail.com>
2025-02-22 21:57:46 +0100

    feat: airflow dag example

--- k8s/airflow/dags/example.py
+++ k8s/airflow/dags/example.py
@@ -1,12 +1,38 @@
-import datetime
-
+from datetime import datetime
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from airflow.models import Param
 
-my_dag = DAG(
-    dag_id="fosscut_example_dag",
-    start_date=datetime.datetime(2021, 1, 1),
-    schedule="@daily",
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0
+}
+
+parameterized_sleep_dag = DAG(
+    'parameterized_sleep',
+    default_args = default_args,
+    description = 'A DAG that sleeps for a parameterized duration',
+    schedule_interval = None,  # Only triggered manually
+    start_date = datetime(2024, 1, 1),
+    catchup = False,
+    params = {
+        "sleep_seconds": Param(
+            default=10,
+            type="integer",
+            minimum=1,
+            maximum=3600,  # Max 1 hour
+            description="Number of seconds to sleep"
+        )
+    }
 )
 
-EmptyOperator(task_id="task", dag=my_dag)
+sleep_task = BashOperator(
+    task_id='sleep_task',
+    bash_command = 'sleep {{ params.sleep_seconds }}',
+    executor_config = {
+        "pod_template_file": os.path.join(AIRFLOW_HOME, "pod_templates/basic_template.yaml")
+    }
+)
