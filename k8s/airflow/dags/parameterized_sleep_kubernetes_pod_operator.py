84fe3e887f61ed077082178563030511584aeccd
karolstepanienko <karol.stepanienko@gmail.com>
2025-02-23 00:50:52 +0100

    feat: airflow dag example

--- k8s/airflow/dags/parameterized_sleep_kubernetes_pod_operator.py
+++ k8s/airflow/dags/parameterized_sleep_kubernetes_pod_operator.py
@@ -35,6 +35,6 @@ KubernetesPodOperator(
     image="karolstepanienko/fosscut-cli-native:0.0.1",
     dag = parameterized_sleep_kubernetes_pod_operator_dag,
     cmds=["bash", "-cx"],
-    arguments=["/fosscut", "&&", "echo", "10"],
+    arguments=["/fosscut", "&&", "sleep", "{{ params.sleep_seconds }}"],
     task_id="sleep_task_kubernetes_pod_operator"
 )
