26ee218cd4241735ce3635ca15bf35f4c79f6465
karolstepanienko <karol.stepanienko@gmail.com>
2025-02-23 02:04:38 +0100

    feat: airflow dag example

--- k8s/airflow/dags/parameterized_sleep_kubernetes_pod_operator.py
+++ k8s/airflow/dags/parameterized_sleep_kubernetes_pod_operator.py
@@ -12,29 +12,73 @@ default_args = {
     'retries': 0
 }
 
-parameterized_sleep_kubernetes_pod_operator_dag = DAG(
-    'parameterized_sleep_kubernetes_pod_operator_dag',
+fosscut_generate_kubernetes_pod_operator_dag = DAG(
+    'fosscut_generate_kubernetes_pod_operator_dag',
     default_args = default_args,
     description = 'A DAG that sleeps for a parameterized duration',
     schedule_interval = None,  # Only triggered manually
     start_date = datetime(2024, 1, 1),
     catchup = False,
     params = {
-        "sleep_seconds": Param(
-            default=10,
-            type="integer",
-            minimum=1,
-            maximum=3600,  # Max 1 hour
-            description="Number of seconds to sleep"
+        "subcommand": Param(
+            default = "foo",
+            enum = ["cg", "ffd", "greedy"],
+            description = "Subcommand that determines cutting plan generation algorithm."
+        ),
+        "redis_url": Param(
+            default = "",
+            type = "string"
         )
     }
 )
 
+secret_volumes = [
+    k8s.V1Volume(
+        name = 'keystore',
+        secret = k8s.V1SecretVolumeSource(
+            secret_name = 'fosscut-keystore'
+        )
+    ),
+    k8s.V1Volume(
+        name = 'truststore',
+        secret = k8s.V1SecretVolumeSource(
+            secret_name = 'fosscut-truststore'
+        )
+    ),
+    k8s.V1Volume(
+        name = 'redis-connection-secrets',
+        secret = k8s.V1SecretVolumeSource(
+            secret_name = 'fosscut-cli-redis-connection-secrets'
+        )
+    )
+]
+
+volume_mounts = [
+    k8s.V1VolumeMount(
+        name='keystore',
+        mount_path='/keystore.p12',
+        read_only=True
+    ),
+    k8s.V1VolumeMount(
+        name='truststore',
+        mount_path='/truststore.p12',
+        read_only=True
+    ),
+    k8s.V1VolumeMount(
+        name='redis-connection-secrets',
+        mount_path='/redis-connection-secrets.yaml',
+        read_only=True
+    )
+]
+
 KubernetesPodOperator(
-    name="sleep_task",
-    image="karolstepanienko/fosscut-cli-native:0.0.1",
-    dag = parameterized_sleep_kubernetes_pod_operator_dag,
-    cmds=["bash", "-cx"],
-    arguments=["/fosscut && sleep {{ params.sleep_seconds }}"],
-    task_id="sleep_task_kubernetes_pod_operator"
+    name = "generate_task",
+    image = "karolstepanienko/fosscut-cli-native:0.0.1",
+    dag = fosscut_generate_kubernetes_pod_operator_dag,
+    cmds = ["bash", "-cx"],
+    arguments = ["/fosscut --redis-connection-secrets /redis-connection-secrets.yaml {{ params.subcommand }} {{ params.redis_url}}"],
+    task_id = "fosscut_generate_kubernetes_pod_operator_task_id",
+    namespace = "fosscut",
+    volumes = secret_volumes,
+    volume_mounts = volume_mounts,
 )
