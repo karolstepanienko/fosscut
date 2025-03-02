2731864fb47258af83ec9bc1d7db01fda69ed1e7
karolstepanienko <karol.stepanienko@gmail.com>
2025-03-02 21:59:00 +0100

    feat: k8s: airflow static webserver secret key

--- k8s/airflow/install.sh
+++ k8s/airflow/install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# static webserver secret key ensures that Airflow components only restart when
+# necessary
+kubectl delete secret airflow-webserver-secret-key -n airflow
+kubectl create secret generic airflow-webserver-secret-key -n airflow --from-literal="webserver-secret-key=$(python3 -c 'import secrets; print(secrets.token_hex(16))')"
+
 # HTTPS secrets
 kubectl delete secret airflow-webserver-tls-secret -n airflow
 kubectl create secret generic airflow-webserver-tls-secret -n airflow --from-file=tls.crt=../../helm/secrets/server.crt --from-file=tls.key=../../helm/secrets/server.key
