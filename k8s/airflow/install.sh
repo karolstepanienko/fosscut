2443859da13d57d7d66122931282026a2cf14fbe
karolstepanienko <karol.stepanienko@gmail.com>
2025-03-08 20:49:39 +0100

    feat: k8s: airflow ingress

--- k8s/airflow/install.sh
+++ k8s/airflow/install.sh
@@ -9,8 +9,13 @@ kubectl create secret generic airflow-webserver-secret-key -n airflow --from-lit
 kubectl delete secret airflow-webserver-tls-secret -n airflow
 kubectl create secret generic airflow-webserver-tls-secret -n airflow --from-file=tls.crt=../../helm/secrets/server.crt --from-file=tls.key=../../helm/secrets/server.key
 
+# fosscut CA cert
+kubectl delete secret fosscut-ca -n airflow
+kubectl create secret generic fosscut-ca -n airflow --from-file=tls.crt=../../helm/secrets/ca.crt
+
+# Ingress secrets
+kubectl delete secret airflow.fosscut.com-tls-secret -n airflow
+kubectl create secret tls airflow.fosscut.com-tls-secret -n airflow --cert=../../helm/secrets/ingress.crt --key=../../helm/secrets/ingress.key
+
 # airflow
 helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace -f local-values.yaml -f values.yaml
-
-# Forward Airflow UI to local machine
-kubectl port-forward svc/airflow-webserver 6080:8080 --namespace airflow
