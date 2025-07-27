bf58e8fdfbcbdd2ec5ce678389fee707170935e4
karolstepanienko <karol.stepanienko@gmail.com>
2025-07-27 13:29:24 +0200

    feat: k8s: tekton and redis reinstall

--- k8s/redis/install.sh
+++ k8s/redis/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+kubectl create namespace redis
+
 # password secret
 kubectl delete secret redis-password-secret -n redis
 kubectl create secret generic redis-password-secret -n redis --from-file=redis-password=../../helm/secrets/redis-password
@@ -10,4 +12,4 @@ kubectl create secret generic redis-tls-secret -n redis --from-file=tls.crt=../.
 
 # redis
 helm template redis bitnami/redis -n redis -f local-values.yaml -f values.yaml > template.log
-helm upgrade --install redis bitnami/redis -n redis --create-namespace --version 20.6.1 -f local-values.yaml -f values.yaml
+helm upgrade --install redis bitnami/redis -n redis --version 20.6.1 -f local-values.yaml -f values.yaml
--- k8s/tekton/install.sh
+++ k8s/tekton/install.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
 helm template tekton . -n tekton -f values.yaml > template.log
 helm upgrade --install tekton . -n tekton --create-namespace -f values.yaml
