5909220fb43e1a28281768550e0fc707eb20367f
karolstepanienko <karol.stepanienko@gmail.com>
2026-01-18 18:03:17 +0100

    perf: compare cicd: airflow DAG

---
+++ k8s/airflow/dags/fosscut_cicd_performance_kubernetes_executor.py
@@ -0,0 +1,52 @@
+import os
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+from kubernetes.client import models as k8s
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0
+}
+
+fosscut_cicd_performance_kubernetes_executor = DAG(
+    'fosscut_cicd_performance_kubernetes_executor',
+    default_args = default_args,
+    description = 'A DAG used in CICD Performance testing. Creates only one pod.',
+    schedule_interval = None,  # Only triggered manually
+    start_date = datetime(2024, 1, 1),
+    catchup = False,
+    params = {}
+)
+
+
+pod_override = k8s.V1Pod(
+    spec = k8s.V1PodSpec(
+        containers = [
+            k8s.V1Container(
+                name = "base",
+                image = "karolstepanienko/fosscut-cli-native:0.0.1",
+                # entrypoint borrowed from the original airflow/airflow Dockerfile
+                command = [
+                    "/usr/bin/dumb-init",
+                    "--",
+                    "/entrypoint"
+                ]
+            )
+        ]
+    )
+)
+
+BashOperator(
+    task_id = 'fosscut_cicd_performance_kubernetes_executor_task_id',
+    bash_command = """
+        #!/bin/sh
+        echo "CICD performance test."
+    """,
+    dag = fosscut_cicd_performance_kubernetes_executor,
+    executor_config = { "pod_override": pod_override }
+)
